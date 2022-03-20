2e6450cf4a8fb26473d399ecaec4e326097ee583
Vinit Tomar <32155118+VinitTomar@users.noreply.github.com>
2022-03-20 20:03:29 +0530

    validation final

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    let str: any = 'Hello Vinit!';
+    const str = 'Hello Vinit!';
     return str;
   }
 }
