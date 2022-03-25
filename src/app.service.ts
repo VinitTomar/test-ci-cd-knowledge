457a61205798bc8a9b005ad619dc2550191b5465
Vinit Tomar <32155118+VinitTomar@users.noreply.github.com>
2022-03-25 17:57:08 +0530

    welcome msg updated
    Welcome message updated to" Hello Vinit. This is version 0.0.14!"

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    const str = 'Hello Vinit!';
+    const str = 'Hello Vinit. This is version 0.0.14!';
     return str;
   }
 }
